1cb1c9c9ca5103d9c0f133de54856f44d3f11701
Mikael Ravantti <Mikael.Ravantti@gmail.com>
2019-09-13 10:09:56 +0200

    Added navbar component

---
+++ src/Components/Navbar/index.js
@@ -0,0 +1,16 @@
+import React, { Component, Fragment } from 'react';
+import './Navbar.css';
+
+export class Navbar extends Component {
+    render() {
+        return(
+            <Fragment>
+                <div className="navbar">
+
+                </div>
+            </Fragment>
+        )
+    }
+}
+
+export default Navbar;
