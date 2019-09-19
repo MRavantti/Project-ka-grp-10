0d19e00a511a1072c5bc8e2cc7a6d348134166b2
Mikael Ravantti <Mikael.Ravantti@gmail.com>
2019-09-19 15:26:20 +0200

    Started with category page and the hero for that page

---
+++ src/Components/CategoryPageHero/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class CategoryPageHero extends Component {
+    Capitalize(str) {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+    render() {
+        const { category } = this.props
+
+        return (
+            <div className="category-page-hero">
+                <h1>{this.Capitalize(category)}</h1>
+                {
+                    category == "dans"
+                        ? <p>ex “Från koreografi till samtal om salsa - här finner du allt material kopplat till dans”</p>
+                        : category == "film"
+                            ? <p>film beskrivning</p>
+                            : category == "musik"
+                                ? <p>musik beskrivning</p>
+                                : <p>teater beskrivning</p>
+
+                }
+            </div>
+        )
+    }
+}
+
+export default CategoryPageHero;
