fcab53466ca662eab60e49c67c042b8ab26f77ae
WebDevIsac <isaclarsson98@gmail.com>
2019-09-21 17:26:03 +0200

    Audio player is working with basic controls

--- src/Components/SoundCloudPlayer/index.js
+++ src/Components/SoundCloudPlayer/index.js
@@ -6,26 +6,48 @@ import { togglePlay } from '../../functions/scPlayer';
 
 
 const SoundCloudPlayer = withCustomAudio(props => {
-    const { trackTitle } = props;
+    const { soundCloudAudio, trackTitle } = props;
+    const audio = soundCloudAudio.audio;
+
+    // console.log(audio.currentTime/audio.duration);
+    // console.log(audio.currentTime);
+    
+    const replay = () => {
+        audio.currentTime -= 30;
+    }
+
+    const forward = () => {
+        audio.currentTime += 30;
+    }
+
+    const setCurrentTime = (e) => {
+        audio.currentTime = audio.duration * e;
+    }
 
     return (
         <div className="soundcloud-player">
             <Progress
-                className="progress"
+                className="progress-container"
+                innerClassName="progress"
+                value={(audio.currentTime/audio.duration)*100}
+                onSeekTrack={setCurrentTime}
             />
             <div className="controls">
                 <div className="control-buttons">
                     <PrevButton
-                        {...props}
+                        onPrevClick={replay}
                         className="prev-button"
+                        // {...props}
                     />
                     <PlayButton
-                    {...props}
-                    className="play-button"
+                        className="play-button"
+                        {...props}
                     />
                     <NextButton
+                        onNextClick={forward}
                         className="next-button"
-                        />
+                        // {...props}
+                    />
                 </div>
                 <h4>{trackTitle}</h4>
                 <Timer {...props}/>
