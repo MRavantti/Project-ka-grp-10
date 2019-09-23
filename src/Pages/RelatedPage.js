e3738b6965d8d6402af401273fb7d617943f0ada
Mikael Ravantti <Mikael.Ravantti@gmail.com>
2019-09-23 13:47:16 +0200

    Added CourseBox and set it up with props to fill out the content of each course

--- src/Pages/RelatedPage.js
+++ src/Pages/RelatedPage.js
@@ -3,6 +3,7 @@ import './style.css'
 
 import BackButton from '../Components/BackButton';
 import BackButtonImage from '../Images/back-button.svg'
+import CourseBox from '../Components/CourseBox';
 
 
 const RelatedPage = (props) => {
@@ -12,6 +13,20 @@ const RelatedPage = (props) => {
             <div className="course-leader">
                 <h3>Pia Olby</h3>
                 <p>Kurser & föreläsningar</p>
+            </div>
+            <div>
+                <CourseBox 
+                courseName="Sånggestaltning" 
+                courseExcerpt="Den här kursen i sånggestaltning erbjuder professionella skådespelare möjlighet att arbeta med och utveckla sin egen vokala och musikaliska potential - och att utmana sig själva sångmässigt lokjahdkhadhjkahkdhkahjdhajkdhakj."
+                />
+                <CourseBox
+                    courseName="Kursnamn B"
+                    courseExcerpt="Den här kursen i sånggestaltning erbjuder professionella skådespelare möjlighet att arbeta med och utveckla sin egen vokala och musikaliska potential - och att utmana sig själva sångmässigt lokjahdkhadhjkahkdhkahjdhajkdhakj."
+                />
+                <CourseBox
+                    courseName="Kursnamn C"
+                    courseExcerpt="Den här kursen i sånggestaltning erbjuder professionella skådespelare möjlighet att arbeta med och utveckla sin egen vokala och musikaliska potential - och att utmana sig själva sångmässigt lokjahdkhadhjkahkdhkahjdhajkdhakj."
+                />
             </div>
         </Fragment>
     )
