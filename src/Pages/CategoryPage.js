b32af7ff832a4b8b18c7c6825cf5467f018f4820
WebDevIsac <isaclarsson98@gmail.com>
2019-09-26 16:16:23 +0200

    Change hero image and set up categorypage filter functions

--- src/Pages/CategoryPage.js
+++ src/Pages/CategoryPage.js
@@ -9,16 +9,45 @@ const CategoryPage = (props) => {
 
 	const category = props.match.params.categoryId;
 
-	let categoryVideos = videos.filter(item => item.tags && item.tags.filter(tag => tag.toLowerCase().includes(category)))
+	let categoryVideos = videos.filter(item => {
+		if (item.tags) {
+			let tagIncludes;
+			item.tags.map(tag => {
+				if (tag.toLowerCase().includes(category)) {
+					tagIncludes = true;
+				}
+			})
+			if (tagIncludes) {
+				return item;
+			}
+		}
+	})
 
-	console.log(categoryVideos);
+	categoryVideos = categoryVideos.length < 15 ? videos : categoryVideos;
+	
+	let categoryPodcasts = podcasts.filter(item => {
+		if (item.tags) {
+			let tagIncludes;
+			item.tags.map(tag => {
+				if (tag.toLowerCase().includes(category)) {
+					tagIncludes = true;
+				}
+			})
+			if (tagIncludes) {
+				return item;
+			}
+		}
+	})
+	
+	categoryPodcasts = categoryPodcasts.length < 30 ? podcasts : categoryPodcasts;
+
 
     return (
         <Fragment>
             <CategoryPageHero category={category} goBack={props.history.goBack}/>
-            <ContentContainer size="small" text="Senaste Podcasts" content={podcasts.slice(50, 65)}/>
-            <ContentContainer size="small" text="Senaste Videoföreläsningar" content={videos.slice(1, 16)}/>
-            <ContentContainer size="small" text="Kategori Namn" content={podcasts.slice(65, 80)}/>
+            <ContentContainer size="small" text="Senaste Podcasts" content={categoryPodcasts.slice(0, 15)}/>
+            <ContentContainer size="small" text="Senaste Videoföreläsningar" content={categoryVideos.slice(0, 15)}/>
+            <ContentContainer size="small" text="Kategori Namn" content={categoryPodcasts.slice(15, 30)}/>
         </Fragment>
     );
 };
