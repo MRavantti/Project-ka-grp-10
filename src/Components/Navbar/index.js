3b37443725e2c2031631cfc627d699cf417145a2
WebDevIsac <isaclarsson98@gmail.com>
2019-09-26 22:19:12 +0200

    Styling navbar and mobile menu

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -2,23 +2,41 @@ import React, { Component, Fragment } from 'react';
 import './style.css';
 
 import MobileMenu from '../MobileMenu';
-import NavbarItems from '../NavbarItems'
+import NavbarItems from '../NavbarItems';
+import SearchBar from '../SearchBar';
 
 export class Navbar extends Component {
 
     state = {
-        menuVisable: false
+		menuVisable: false,
+		searchVisable: false,
     }
 
     toggleMenu = () => {
         this.setState(prevState => ({
-            menuVisable: !prevState.menuVisable
-        }))
-    }
+			searchVisable: false,
+			menuVisable: !prevState.menuVisable,
+		}))
+	}
+	
+	toggleSearch = () => {
+		this.setState(prevState => ({
+			menuVisable: false,
+			searchVisable: !prevState.searchVisable,
+		}))
+	}
+
+	hideSearch = () => {
+		console.log('click');
+		this.setState(() => ({
+			searchVisable: false
+		}))
+	}
 
     render() {
         return (
             <Fragment>
+				<SearchBar class={this.state.searchVisable ? "visible" : null}/>
                 {
                     this.state.menuVisable
                         ? <Fragment>
@@ -29,7 +47,7 @@ export class Navbar extends Component {
                         </Fragment>
                         
                         : <div className="navbar">
-                            <NavbarItems toggleMenu={this.toggleMenu} />
+                            <NavbarItems toggleMenu={this.toggleMenu} toggleSearch={this.toggleSearch} searchVisible={this.state.searchVisable} hideSearch={this.hideSearch}/>
                         </div>
                 }
             </Fragment>
--- src/Components/NavbarItems/index.js
+++ src/Components/NavbarItems/index.js
@@ -5,10 +5,15 @@ import './style.css';
 export class NavbarItems extends Component {
     render() {
         const { toggleMenu } = this.props
+        const { toggleSearch } = this.props
+        const { hideSearch } = this.props
+		const { searchVisible } = this.props
+
         return (
             <Fragment>
-                <img className="search-icon navbar-item" src="/assets/svgs/search.svg" alt="search icon" />
-                <Link to="/"><img className="logo navbar-item" src="/assets/svgs/k-play.svg" alt="logo" /></Link>
+                {searchVisible ? <img onClick={toggleSearch} className="navbar-item" src="/assets/svgs/cross.svg" alt="search icon" /> 
+				: <img onClick={toggleSearch} className="search-icon navbar-item" src="/assets/svgs/search.svg" alt="search icon"/>}
+                <Link onClick={hideSearch} to="/"><img className="logo navbar-item" src="/assets/svgs/k-play.svg" alt="logo" /></Link>
                 <img onClick={ toggleMenu } className="burger-icon navbar-item" src="/assets/svgs/menu.svg" alt="buger icon" />
             </Fragment>
         )
